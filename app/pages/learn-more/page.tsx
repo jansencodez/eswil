0d11951717d01f4055f9656354f1409cd26ac19f
jansencodez <nyamwecliff@gmail.com>
2024-10-13 01:28:29 +0300

    minor changes

---
+++ app/pages/learn-more/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <section>
+        <h1>About us</h1>
+        <section>
+          <h2>Our Location</h2>
+          <p>
+            We are located along Mumias-Busia Road, at Munami Market; Kakamega/Busia county border.
+          </p>
+          <p>
+            TEPS follows
+          </p>
+        </section>
+      </section>
+    </div>
+  )
+}
+
+export default page
